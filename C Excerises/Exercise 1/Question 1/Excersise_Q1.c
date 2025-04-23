c71d78a2e82871cfd503c558485c26d86401ca0a
Hani Hatim <143106638+HaniHatim@users.noreply.github.com>
2025-04-24 01:13:44 +0400

    Delete C Excerises/Exercise 1 directory

--- C Excerises/Exercise 1/Question 1/Excersise_Q1.c
+++
@@ -1,9 +0,0 @@
-// Write a program to display the shape (page 32 from C programming: a modern approach)
-
-#include <stdio.h>
-
-int main(void)
-{
-    printf("        *\n       *\n      *\n*    *\n *  *\n   *\n");
-    return 0;
-}
