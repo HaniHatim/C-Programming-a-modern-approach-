5487a18921a0250376002610ee52d922669e575f
Hani Hatim <143106638+HaniHatim@users.noreply.github.com>
2025-04-24 01:12:39 +0400

    Add files via upload

---
+++ C Excerises/Exercise 1/Question 2/Excersise_Q2.c
@@ -0,0 +1,18 @@
+//Write a program that computes the volume of a sphere with a 10-meter radius, using the formula v = 4/3πr^3
+
+#include <stdio.h>
+
+int main(void)
+{
+    float volume, formula;
+    float fraction = 4.0f/3.0f;
+    float pi = 3.14f;
+    float radius = 10.0f;
+
+    formula = (fraction) * pi * radius;
+
+    printf("%.2f", formula);
+
+    return 0;
+
+}
