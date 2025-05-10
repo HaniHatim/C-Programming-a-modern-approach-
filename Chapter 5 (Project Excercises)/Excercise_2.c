f4312a05571e7523192834c4e2c489bfb304a13d
Hani Hatim <143106638+HaniHatim@users.noreply.github.com>
2025-05-11 03:45:11 +0400

    Update Excercise_2.c

--- Chapter 5 (Project Excercises)/Excercise_2.c
+++ Chapter 5 (Project Excercises)/Excercise_2.c
@@ -7,13 +7,13 @@ int main(void)
     printf("Enter the time in 24:00 format: ");
     scanf("%2d:%2d", &hour, &min);
 
-    if (hour > 23 || hour < 00 || min > 60 || min < 00)
+    if (hour > 23 || hour < 0 || min > 59 || min < 0)
     {
         printf("Invlid format entered\n");
         return 0;
     }
 
-    if (hour <= 12 && hour != 00)
+    if (hour <= 12 && hour != 0)
     {
         printf("The time is %.2d:%.2d AM\n", hour, min);
     }
@@ -22,7 +22,7 @@ int main(void)
         hour = hour - 12;
         printf("The time is %.2d:%.2d PM\n", hour, min);
     }
-    else if (hour == 00)
+    else if (hour == 0)
     {
         hour = 12;
         printf("The time is %.2d:%.2d PM\n", hour, min);
