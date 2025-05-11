f463a3ecf4d59020466eff25a4f5bb104d0e6b03
Hani Hatim <143106638+HaniHatim@users.noreply.github.com>
2025-05-11 16:38:02 +0400

    Add files via upload

---
+++ Chapter 5 (Project Excercises)/Excercise_7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3, n4;
+    int largest, smallest;
+
+    printf("Please enter four integers: ");
+    scanf("%d%d%d%d", &n1, &n2, &n3, &n4);
+
+    largest = n1;
+    smallest = n1;
+
+    if (n2 > largest)
+    largest = n2;
+    
+    else if (n2 < smallest)
+    smallest = n2;
+
+    if (n3 > largest)
+    largest = n3;
+    
+    else if (n3 < smallest)
+    smallest = n3;
+
+    if (n4 > largest)
+    largest = n4;
+
+    else if (n4 < smallest)
+    smallest = n4;
+
+
+    printf("The Largest number is: %d\n", largest);
+    printf("The smallest number is: %d\n", smallest);
+
+    return 0;
+
+}
