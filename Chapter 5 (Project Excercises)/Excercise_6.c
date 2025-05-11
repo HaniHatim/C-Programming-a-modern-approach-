fb3b10563e7a88c43d692b66079d8e8280955465
Hani Hatim <143106638+HaniHatim@users.noreply.github.com>
2025-05-11 17:30:18 +0400

    Update Excercise_6.c

--- Chapter 5 (Project Excercises)/Excercise_6.c
+++ Chapter 5 (Project Excercises)/Excercise_6.c
@@ -1,4 +1,7 @@
-// The digits 0 13800 15173 5
+/*
+Modify the upc.c program of Section 4.1 so that it checks whether a UPC is valid. After
+the user enters a UPC, the program will display either VALID or NOT VALID
+*/
 
 #include <stdio.h>
 
